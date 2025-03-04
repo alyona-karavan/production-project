6d169f78b886989606032ec9f0203499d8f6ff3f
Mac <mac@MacBook-Pro-Mac.local>
2025-03-04 12:49:00 +0600

    48 ArticlesPage и ArticleDetailsPage. Декомпозиция. Сущности. Webpack publicPath

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -16,6 +16,7 @@ export function buildWebpackConfig(options: BuildOptions)
             path: paths.build,
             filename: '[name][contenthash].js',
             clean: true,
+            publicPath: '/',
         },
         plugins: buildPlugins(options),
         module: {
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -4,6 +4,8 @@ import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import { ProfilePage } from 'pages/ProfilePage';
+import { ArticlesPage } from 'pages/ArticlesPage';
+import { ArticleDetailsPage } from 'pages/ArticleDetailsPage';
 
 export type AppRoutesProps = RouteProps & {
     authOnly?: boolean;
@@ -13,7 +15,10 @@ export enum AppRoutes {
     MAIN = 'main',
     ABOUT = 'about',
     PROFILE = 'profile',
+    ARTICLES = 'articles',
+    ARTICLE_DETAILS = 'article_details',
 
+    // last
     NOT_FOUD = 'not_found',
 }
 
@@ -21,6 +26,9 @@ export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
     [AppRoutes.PROFILE]: '/profile',
+    [AppRoutes.ARTICLES]: '/articles',
+    [AppRoutes.ARTICLE_DETAILS]: '/articles/', // + :id
+    // last
     [AppRoutes.NOT_FOUD]: '*',
 };
 
@@ -36,6 +44,16 @@ export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
     [AppRoutes.PROFILE]: {
         path: RoutePath.profile,
         element: <ProfilePage />,
+        authOnly: true,
+    },
+    [AppRoutes.ARTICLES]: {
+        path: RoutePath.articles,
+        element: <ArticlesPage />,
+        authOnly: true,
+    },
+    [AppRoutes.ARTICLE_DETAILS]: {
+        path: `${RoutePath.article_details}:id`,
+        element: <ArticleDetailsPage />,
         authOnly: true,
     },
     [AppRoutes.NOT_FOUD]: {
