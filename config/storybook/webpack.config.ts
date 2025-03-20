565edb924f598abd24dd849efe1400a976b00d44
AlyonaKaravan <alyonakaravan99@gmail.com>
2025-03-20 15:42:43 +0600

    13. Арх-ые правила eslint. 88 Алиасы в сторибуке. Приводим сторибук в порядок. Сторибук пока не прогружается

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,11 +1,9 @@
-// @ts-ignore
 import webpack, { DefinePlugin, RuleSetRule } from 'webpack';
 import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
-export default ({ config: originalConfig }: {config: webpack.Configuration}) => {
-    const config = { ...originalConfig };
+export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
         build: '',
         html: '',
@@ -14,16 +12,16 @@ export default ({ config: originalConfig }: {config: webpack.Configuration}) => 
         locales: '',
         buildLocales: '',
     };
-    if (config.resolve) {
-        // @ts-ignore
-        config.resolve.modules.push(paths.src);
-        // @ts-ignore
-        config.resolve.extensions.push('.ts', '.tsx');
-    }
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.extensions!.push('.ts', '.tsx');
+    config!.resolve!.alias = {
+        ...config!.resolve!.alias,
+        '@': paths.src,
+    };
 
-    // @ts-ignore
     // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+    // @ts-ignore
+    config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
@@ -31,16 +29,13 @@ export default ({ config: originalConfig }: {config: webpack.Configuration}) => 
         return rule;
     });
 
-    // @ts-ignore
-    config.module.rules.push({
+    config!.module!.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    // @ts-ignore
-    config.module.rules.push(buildCssLoader(true));
+    config!.module!.rules.push(buildCssLoader(true));
 
-    // @ts-ignore
-    config.plugins.push(new DefinePlugin({
+    config!.plugins!.push(new DefinePlugin({
         __IS_DEV__: JSON.stringify(true),
         __API__: JSON.stringify('https://testapi.ru'),
         __PROJECT__: JSON.stringify('storybook'),
--- src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import withMock from 'storybook-addon-mock';
 import { NotificationList } from './NotificationList';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
     title: 'entities/Notification//NotificationList',
@@ -9,9 +10,27 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [withMock],
 } as ComponentMeta<typeof NotificationList>;
 
 const Template: ComponentStory<typeof NotificationList> = (args) => <NotificationList {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
+Normal.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/notifications`,
+            method: 'GET',
+            status: 200,
+            response: [
+                {
+                    id: '1',
+                    title: 'Уведомление',
+                    description: 'Описание уведомления',
+                },
+            ],
+        },
+    ],
+};
