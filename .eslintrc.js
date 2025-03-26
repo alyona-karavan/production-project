db06edf7190871ca63db1c00249c87cadbe912c8
AlyonaKaravan <alyonakaravan99@gmail.com>
2025-03-26 11:59:19 +0600

    14/117 Prettier для форматирования кода. Интегрируем с eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
         'plugin:react/recommended',
         'airbnb',
         'plugin:i18next/recommended',
+        'plugin:storybook/recommended',
+        'prettier',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -23,23 +25,22 @@ module.exports = {
         'i18next',
         'react-hooks',
         'ulbi-tv-plugin',
-        'unused-imports'
+        'unused-imports',
     ],
     rules: {
-        'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props': [2, 4],
         'unused-imports/no-unused-imports': 'error',
-        indent: [2, 4],
         'react/jsx-filename-extension': [
             2,
-            { extensions: ['.js', '.jsx', '.tsx'] },
+            {
+                extensions: ['.js', '.jsx', '.tsx'],
+            },
         ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'off',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
-        'react/jsx-props-no-spreading': 'off',
+        'react/jsx-props-no-spreading': 'warn',
         'react/function-component-definition': 'off',
         'no-shadow': 'off',
         'import/extensions': 'off',
@@ -50,30 +51,42 @@ module.exports = {
             {
                 markupOnly: true,
                 ignoreAttribute: [
+                    'as',
+                    'role',
                     'data-testid',
                     'to',
                     'target',
                     'justify',
                     'align',
+                    'border',
                     'direction',
                     'gap',
-                    'role',
-                    'as',
-                    'fallback',
-                    'border',
                 ],
             },
         ],
-        'max-len': ['error', { ignoreComments: true, code: 125 }],
+        'max-len': [
+            'error',
+            {
+                ignoreComments: true,
+                code: 125,
+            },
+        ],
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
-        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
+        'react-hooks/rules-of-hooks': 'error',
+        // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'error',
+        // Checks effect dependencies,
         'no-param-reassign': 'off',
         'no-undef': 'off',
-        'no-console': 'off',
         'react/no-array-index-key': 'off',
-        'ulbi-tv-plugin/path-checker': ['error', { alias: '@' }],
+        'arrow-body-style': 'off',
+        'ulbi-tv-plugin/path-checker': [
+            'error',
+            {
+                alias: '@',
+            },
+        ],
         'ulbi-tv-plugin/layer-imports': [
             'error',
             {
@@ -85,11 +98,14 @@ module.exports = {
             'error',
             {
                 alias: '@',
-                testFilesPatterns: ['**/*.test.*', '**/*.story.*', '**/StoreDecorator.tsx'],
+                testFilesPatterns: [
+                    '**/*.test.*',
+                    '**/*.story.*',
+                    '**/StoreDecorator.tsx',
+                ],
             },
         ],
-        'arrow-body-style': 'off',
-        'func-names': 'off',
+        'react/jsx-max-props-per-line': ['error', { maximum: 4 }],
     },
     globals: {
         __IS_DEV__: true,
