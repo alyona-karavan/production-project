5a9d1e0630b5eb1c6527dd289db642f4476a0a85
AlyonaKaravan <alyonakaravan99@gmail.com>
2025-03-29 17:10:32 +0600

    16/136 Sticky Layout для статей. Редизайн табов, фильтров

---
+++ src/shared/ui/redesigned/Input/Input.tsx
@@ -0,0 +1,90 @@
+import React, {
+    InputHTMLAttributes, memo, useEffect, useRef, useState,
+} from 'react';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+import styles from './Input.module.scss';
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'readOnly'>
+
+interface InputProps extends HTMLInputProps {
+    className?: string;
+    value?: string | number;
+    onChange?: (value: string) => void;
+    autofocus?: boolean;
+    readonly?: boolean;
+}
+
+export const Input = memo((props: InputProps) => {
+    const {
+        className,
+        value,
+        onChange,
+        type = 'text',
+        placeholder,
+        autofocus,
+        readonly,
+        ...otherProps
+    } = props;
+    const ref = useRef<HTMLInputElement>(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [caretPosition, setCaretPosition] = useState(0);
+    const isCaretVisible = isFocused && !readonly;
+
+    useEffect(() => {
+        if (autofocus) {
+            setIsFocused(true);
+            ref.current?.focus();
+        }
+    }, [autofocus]);
+
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange?.(e.target.value);
+        setCaretPosition(e.target.value.length);
+    };
+
+    const onBlur = () => {
+        setIsFocused(false);
+    };
+
+    const onFocus = () => {
+        setIsFocused(true);
+    };
+
+    const onSelect = (e: any) => {
+        setCaretPosition(e?.target?.selectionStart || 0);
+    };
+
+    const mods: Mods = {
+        [styles.readOnly]: readonly,
+    };
+
+    return (
+        <div className={classNames(styles.InputWrapper, mods, [className])}>
+            {placeholder && (
+                <div className={styles.placeholder}>
+                    {`${placeholder}>`}
+                </div>
+            )}
+            <div className={styles.caretWrapper}>
+                <input
+                    ref={ref}
+                    type={type}
+                    value={value}
+                    onChange={onChangeHandler}
+                    className={styles.input}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    onSelect={onSelect}
+                    readOnly={readonly}
+                    {...otherProps}
+                />
+                {isCaretVisible && (
+                    <span
+                        className={styles.caret}
+                        style={{ left: `${caretPosition * 9}px` }}
+                    />
+                )}
+            </div>
+        </div>
+    );
+});
